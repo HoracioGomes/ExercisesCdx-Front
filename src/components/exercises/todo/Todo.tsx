94fb844dd92142925f31c1487e87d27cbabda24a
Horacio <oicaroh64@gmail.com>
2023-10-05 17:02:30 -0300

    Delete todo implementation

--- src/components/exercises/todo/Todo.tsx
+++ src/components/exercises/todo/Todo.tsx
@@ -18,6 +18,15 @@ function ExerciseB() {
         )
     })
 
+    async function deleteTodo(id) {
+        try {
+            await api.delete(`todo/delete/${id}`)
+            todos.filter(todo => todo.id !== id)
+        } catch (error) {
+            alert('error when deleting')
+        }
+    }
+
     return (
 
         <div className="todo-container">
@@ -27,23 +36,23 @@ function ExerciseB() {
                 </FiPlus></Link>
             </header>
             <ul className="todo-list">
-               {todos.map(todo =>{
-                const dataCorrigida = todo.dataAgendada.replace('--', '-');
-                const dataSemZ = dataCorrigida.replace('Z', '');
+                {todos.map(todo => {
+                    const dataCorrigida = todo.dataAgendada.replace('--', '-');
+                    const dataSemZ = dataCorrigida.replace('Z', '');
 
-                return(
-                <li>
-                <strong>Title:</strong>
-                <p>{todo.titulo}</p>
-                <strong>Date:</strong>
-                <p>{format(new Date(dataSemZ), 'dd/MM/yyyy')}</p>
-                <strong>Descrition:</strong>
-                <p>{todo.descricao}</p>
-                <button type="button"><FiCheck size={20} color="#251FC5" /></button>
-                <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
-            </li>
-                )
-})}
+                    return (
+                        <li>
+                            <strong>Title:</strong>
+                            <p>{todo.titulo}</p>
+                            <strong>Date:</strong>
+                            <p>{format(new Date(dataSemZ), 'dd/MM/yyyy')}</p>
+                            <strong>Descrition:</strong>
+                            <p>{todo.descricao}</p>
+                            <button type="button"><FiCheck size={20} color="#251FC5" /></button>
+                            <button onClick={() => deleteTodo(todo.id)} type="button"><FiTrash2 size={20} color="#251FC5" /></button>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
