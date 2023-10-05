ba2c79f1da0978e82204e90cea91766b7330bb93
Horacio <oicaroh64@gmail.com>
2023-10-05 16:51:28 -0300

    Todo list implementation

--- src/components/exercises/todo/Todo.tsx
+++ src/components/exercises/todo/Todo.tsx
@@ -1,58 +1,49 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./Todo.css"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FiPlus, FiTrash2, FiCheck } from "react-icons/fi"
+import api from "../../../services/api";
+import { format } from 'date-fns'
+
 
 function ExerciseB() {
+    const nav = useNavigate()
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        api.get('todo').then(
+            response => {
+                setTodos(response.data)
+            }
+        )
+    })
+
     return (
 
-        <div className="todo-container">    
+        <div className="todo-container">
             <header>
                 <h1>Todo</h1>
                 <Link className="btn-add-todo" to="new-todo"><FiPlus className="icon-add-todo" size={30} color="251FC5">
                 </FiPlus></Link>
             </header>
             <ul className="todo-list">
+               {todos.map(todo =>{
+                const dataCorrigida = todo.dataAgendada.replace('--', '-');
+                const dataSemZ = dataCorrigida.replace('Z', '');
+
+                return(
                 <li>
-                    <strong>Title:</strong>
-                    <p>Aqui o Titulo</p>
-                    <strong>Date:</strong>
-                    <p>00/00/00</p>
-                    <strong>Descrition:</strong>
-                    <p>Aqui a descrição do todo</p>
-                    <button type="button"><FiCheck size={20} color="#251FC5" /></button>
-                    <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Aqui o Titulo-B</p>
-                    <strong>Date:</strong>
-                    <p>00/00/01</p>
-                    <strong>Descrition:</strong>
-                    <p>Aqui a descrição do todo-B</p>
-                    <button type="button"><FiCheck size={20} color="#251FC5" /></button>
-                    <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Aqui o Titulo-B</p>
-                    <strong>Date:</strong>
-                    <p>00/00/01</p>
-                    <strong>Descrition:</strong>
-                    <p>Aqui a descrição do todo-B</p>
-                    <button type="button"><FiCheck size={20} color="#251FC5" /></button>
-                    <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Aqui o Titulo-B</p>
-                    <strong>Date:</strong>
-                    <p>00/00/01</p>
-                    <strong>Descrition:</strong>
-                    <p>Aqui a descrição do todo-B</p>
-                    <button type="button"><FiCheck size={20} color="#251FC5" /></button>
-                    <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
-                </li>
+                <strong>Title:</strong>
+                <p>{todo.titulo}</p>
+                <strong>Date:</strong>
+                <p>{format(new Date(dataSemZ), 'dd/MM/yyyy')}</p>
+                <strong>Descrition:</strong>
+                <p>{todo.descricao}</p>
+                <button type="button"><FiCheck size={20} color="#251FC5" /></button>
+                <button type="button"><FiTrash2 size={20} color="#251FC5" /></button>
+            </li>
+                )
+})}
             </ul>
         </div>
     )
