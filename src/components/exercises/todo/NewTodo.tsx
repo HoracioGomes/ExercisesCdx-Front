b7f7be1c4a1f38965f8c51a019fd4f2d2b8228c8
Horacio <oicaroh64@gmail.com>
2023-10-05 14:13:51 -0300

    Todo addition imlementation

--- src/components/exercises/todo/NewTodo.tsx
+++ src/components/exercises/todo/NewTodo.tsx
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Todo.css"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FiArrowLeft } from "react-icons/fi"
+import api from "../../../services/api";
 
 function NewTodo() {
+    const [titulo, setTitulo] = useState('')
+    const [descricao, setDescricao] = useState('')
+    const [date, setDate] = useState('')
+    const nav = useNavigate()
+
+    async function createTodo(e) {
+        e.preventDefault()
+
+        const data = {
+            titulo,
+            descricao,
+            date
+        }
+
+        try {
+            console.log(data);
+            const response = await api.post('todo', data)
+            console.log(response);
+            nav('/todo')
+        } catch (error) {
+            alert('Error while recording todo!')
+        }
+    }
+
     return (
         <div className="new-todo-container">
             <div className="content">
@@ -13,10 +38,10 @@ function NewTodo() {
                         <FiArrowLeft className="icon-confirm-todo" size={16} color="251FC5"></FiArrowLeft></Link>
                 </section>
                 <form>
-                    <input placeholder="Title" />
-                    <input placeholder="Descricao" />
-                    <input type="date" placeholder="Date" />
-                    <button className="button-submit-todo" type="submit">Add</button>
+                    <input value={titulo} onChange={e => setTitulo(e.target.value)} placeholder="Title" />
+                    <input value={descricao} onChange={e => setDescricao(e.target.value)} placeholder="Description" />
+                    <input value={date} onChange={e => setDate(e.target.value)} type="date" placeholder="Date" />
+                    <button onClick={createTodo} className="button-submit-todo" type="submit">Add</button>
                 </form>
             </div>
         </div>
